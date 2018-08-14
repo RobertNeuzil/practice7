7e9ff8d2f8155e1d66ed7afd46c9bc23c8cb4061
Robert Neuzil <robertneuzil@gmail.com>
2018-08-14 00:46:34 -0400

    ran a loop through the items, comparing completion boolean

--- todolistapplication/todo/models.py
+++ todolistapplication/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Todo(models.Model):
+	text = models.CharField(max_length=40)
+	completed = models.BooleanField(default=False)
+
+	def __str__ (self):
+		return self.text
+
+
 # Create your models here.
--- todolistapplication/todo/views.py
+++ todolistapplication/todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Todo
+
+
 # Create your views here.
 
 def index(request):
-	context = { }
+	to_do_list = Todo.objects.order_by("id")
+	context = { "to_do_list" : to_do_list }
 	return render(request, "todo/index.html", context)
