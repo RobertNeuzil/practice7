dfd33c63ec0cd836190d913a1e3928118d28a8bd
Robert Neuzil <robertneuzil@gmail.com>
2018-08-17 21:09:27 -0400

    form

--- todoapplication/first/views.py
+++ todoapplication/first/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import ToDo
+from .forms import ToDoForm
+
 
 def index(request):
 	todolist = ToDo.objects.order_by('id')
-	context = {'todolist': todolist}
+	
+	form = ToDoForm()
+
+	context = {'todolist': todolist, 'form': form}
+	
+
+
 	return render(request, 'first/index.html', context)
 
 # Create your views here.
